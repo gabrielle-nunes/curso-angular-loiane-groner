d08d7d70dc4dc10ca1b0ae53498f4cb556d519b2
Gabrielle <gabinunes09@gmail.com>
2022-02-11 15:26:04 -0300

    criando dois componentes

---
+++ primeiro-projeto-angular/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrimeiroComponente } from './primeiro-componente/primeiro-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrimeiroComponente,
+    SegundoComponenteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ primeiro-projeto-angular/src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'primeiro-componente',
+  template: `
+    <p>Primeiro component com Angular</p>
+  `,
+})
+
+export class PrimeiroComponente{
+
+}
