4f7a3cae40be3dd380ec424e55ac6727d143381d
Gabrielle <gabinunes09@gmail.com>
2022-02-22 15:47:32 -0300

    Event binding

--- primeiro-projeto-angular/src/app/data-binding/data-binding.component.ts
+++ primeiro-projeto-angular/src/app/data-binding/data-binding.component.ts
@@ -3,25 +3,51 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+      .highlight {
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
-
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem: string = 'https://thiscatdoesnotexist.com';
 
-  getValor(){
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+
+  getValor() {
     return 1;
   }
 
-  getCurso(){
+  getCurso() {
     return true;
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  botaoClicado() {
+    alert('Deu certo :)');
   }
 
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
